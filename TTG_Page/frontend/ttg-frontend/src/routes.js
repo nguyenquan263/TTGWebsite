4f107ec72a1fbaf5a2a73b670191d5e810ec1d99
Quan Nguyen <nguyenquan263@gmail.com>
2019-12-25 20:59:28 -0600

    Move to Vue CLI 3.

--- TTG_Page/frontend/ttg-frontend/src/routes.js
+++
@@ -1,7 +0,0 @@
-import HomeComponent from './components/client/Home.vue';
-const Bar = { template: '<div>bar</div>' }
-
-export const routes = [
-    { path: '/bar', component: Bar },
-    { path: '/home', component: HomeComponent }
-];
